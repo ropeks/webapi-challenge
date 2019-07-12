ecf8f3083a15587525ce1efe10d8c24b4beab4a3
ropeks <ropeksmilioner@gmail.com>
2019-07-12 11:05:27 +0200

    create DELETE endpoints

--- api/routes.js
+++ api/routes.js
@@ -29,6 +29,19 @@ router.post('/actions', (req, res) => {
         })
 });
 
+router.delete('/actions/:id', (req, res) => {
+    Actions
+        .remove(req.params.id)
+        .then(data => {
+            res.status(200)
+            .json({ message: 'the action has been deleted' });
+        })
+        .catch(err => {
+            res.status(500)
+                .json({ message: 'cannot delete action' })
+        })
+});
+
 // project routes
 
 router.get('/projects', (req, res) => {
@@ -54,5 +67,19 @@ router.post('/projects', (req, res) => {
                 .json({ message: 'cannot add projects' })
         })
 });
+
+router.delete('/projects/:id', (req, res) => {
+    Projects
+        .remove(req.params.id)
+        .then(data => {
+            res.status(200)
+            .json({ message: 'the project has been deleted' });
+        })
+        .catch(err => {
+            res.status(500)
+                .json({ message: 'cannot delete project' })
+        })
+});
+
 
 module.exports = router;
