620e6cfa8eda5185227a423667070ba080c6409b
ropeks <ropeksmilioner@gmail.com>
2019-07-12 10:22:02 +0200

    set up scripts and express router

---
+++ api/routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const actions = require('../data/helpers/actionModel');
+const route = express.Router();
+
+
+
+module.exports = route;
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const routes = require('./api/routes');
 const server = express();
 
 server.use(express.json());
+
+server.use('/', routes);
 
 server.get('/', (req, res) => {
   res.send('<h1>Server is up and running.</h1>');
